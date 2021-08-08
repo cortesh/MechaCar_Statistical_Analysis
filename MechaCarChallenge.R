e9e0a1e7358166c8d81343762b9ece86228f4484
cortesh <hcortes0725@gmail.com>
2021-08-07 22:07:01 -0400

    deliverable2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,9 @@
 library(jsonlite)
 library(tidyverse)
 
+# ------------------------------------------------------------------------------------------------------
+# deliverable 1
+
 # read MechaCar_mpg as dataframe
 mecha_mpg <- read.csv(file='mECHAcAR_MPG.csv',check.names=F,stringsAsFactors = F)
 
@@ -8,3 +11,21 @@ head(mecha_mpg)
 
 #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_mpg)) 
+
+# ------------------------------------------------------------------------------------------------------
+# deliverable 2 
+
+
+# Suspension_Coil as dataframe
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+head(Suspension_Coil)
+
+# Write an RScript that creates a total_summary dataframe using the summarize() function to get the 
+# mean, median, variance, and standard deviation of the suspension coil's PSI column
+total_summary <- Suspension_Coil  %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) 
+
+
+# Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions 
+# to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column
+lot_summary <- Suspension_Coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) 
